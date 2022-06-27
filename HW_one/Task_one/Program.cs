6b015aebad0c89db65b4491bbd5804830ab244a3
ilemisova <ilemisova@mail.ru>
2022-06-27 17:01:35 +0300

    NEw folder

---
+++ HW_one/Task_one/Program.cs
@@ -0,0 +1,17 @@
+﻿/*задача 1*/
+int a,b,max;
+
+Console.WriteLine("Inpute a first number: ");
+a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inpute a second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+
+if(a > b)
+{
+Console.WriteLine("max " + a);
+}
+{
+Console.WriteLine("max " + b);
+}
