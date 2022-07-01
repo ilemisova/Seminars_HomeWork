229eb2aa15003a05494053215cbcb7e0f5794ff9
ilemisova <ilemisova@mail.ru>
2022-07-01 22:51:56 +0300

    task3 changes

--- HW_two/Task3Seminar2/Program.cs
+++ HW_two/Task3Seminar2/Program.cs
@@ -1,6 +1,6 @@
 ﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным */
 
-void Weekend(int a)
+void Day(int a)
 { 
     if(a == 6 ||a == 7) Console.WriteLine("Weekend ");
     else Console.WriteLine("No Weekend ");
@@ -8,5 +8,5 @@ void Weekend(int a)
 
 Console.WriteLine("Input of day number: " );
 int number = Convert.ToInt32(Console.ReadLine());
-Weekend(number);
+Day(number);
 
