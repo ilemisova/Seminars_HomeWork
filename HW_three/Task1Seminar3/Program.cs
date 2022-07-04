eeaea77dff748c1de03a81dcb1260d7ccd8063f9
ilemisova <ilemisova@mail.ru>
2022-07-05 01:24:07 +0300

    выполнение ДЗ№3

---
+++ HW_three/Task1Seminar3/Program.cs
@@ -0,0 +1,9 @@
+﻿void SelectionSort(int num)
+    {
+	if(num / 10000 == num % 10 && num / 1000 % 10 == (num % 100) / 10) Console.WriteLine("this number is palindrom ");
+	else Console.WriteLine("uncorrect number! ");
+    }
+Console.WriteLine("Input fiveDigit number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+SelectionSort(num);
---
+++ HW_three/Task2Seminar3/Program.cs
@@ -0,0 +1,12 @@
+﻿void ThirdPower(int num)
+{
+    	for(int current = 1; current <= num; current++)
+	    {
+            int cube = current * current * current;
+            Console.WriteLine(cube + " ");
+        }
+}
+Console.WriteLine("Input positive int number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ThirdPower(num);
