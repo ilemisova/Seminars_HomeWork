8bd13b3a04937ee74183c5eddfee8e9a78aa8f4f
ilemisova <ilemisova@mail.ru>
2022-07-10 19:37:26 +0300

    выполнение ДЗ№4

---
+++ HW_four/Task1Seminar4/Program.cs
@@ -0,0 +1,14 @@
+﻿int FindSum(int a)
+{
+	int sum = 0;
+        while(a > 0)
+    	{
+        sum += (a % 10) ;
+        a = a / 10;
+        }
+	return sum;
+}
+Console.WriteLine("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FindSum(num));
