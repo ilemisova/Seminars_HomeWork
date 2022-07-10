d9e3d50dcc512017a441b5dad0e765a55c4d4cc4
ilemisova <ilemisova@mail.ru>
2022-07-10 18:02:30 +0300

    task2

---
+++ HW_four/Task2Seminar4/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[]CreateArray(int size)
+{
+	int[] newArray = new int[size];
+
+	Console.WriteLine("Creating array here: ");
+	for(int i = 0; i < size; i++)
+	{
+		Console.Write($"Input {i + 1} element: ");
+		newArray[i] = Convert.ToInt32(Console.ReadLine());
+	}
+
+	Console.WriteLine();
+	return newArray;
+}
+
+void ShowArray(int[] array)
+{
+	for(int i = 0; i < array.Length; i++)
+		Console.Write(array[i] + " ");
+	Console.WriteLine();
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[]array = CreateArray(size);
+
+ShowArray(array);
