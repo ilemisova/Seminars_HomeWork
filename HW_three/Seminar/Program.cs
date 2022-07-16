761229521376ab298967ff55e298dc1d80429fc7
ilemisova <ilemisova@mail.ru>
2022-07-17 00:54:02 +0300

    save

--- HW_three/Seminar/Program.cs
+++ HW_three/Seminar/Program.cs
@@ -1,5 +1,5 @@
 ﻿//void Triangle (int a, int b, int c)
-{
+/*{
 	if (a < b +c && b < a + c && c < a + b) Console.WriteLine(" ok ");
     else  Console.WriteLine( "No");
 }
@@ -15,7 +15,7 @@ Triangle(num1, num2, num3);
 
 /*Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.*/
 
-int Fibonacci( int n)
+/*int Fibonacci( int n)
 
 0 1 1 2 3 5 8 13 21 34 55
 {
@@ -25,3 +25,4 @@ int Fibonacci( int n)
 
 Console.WriteLine("Input n: ");
 int n = Convert.ToInt32(Console.ReadLine());
+*/
