0e94d3e72ebaaf44033f6a808381f21858e8872f
ilemisova <ilemisova@mail.ru>
2022-07-22 01:02:51 +0300

    HW nine

---
+++ HW_nine/Task2Seminar9/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int SumNums(int m, int n)
+{
+	if(n > m)
+	{
+	   return n + SumNums(m, n-1);
+	}
+    else return n;
+}
+
+Console.WriteLine(SumNums(1,15)); 
